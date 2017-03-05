b02e9cf67784b6180070f0f525037bf5ac6dc5ab
HanikaVats <vatshanika@gmail.com>
2017-03-05 18:34:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 //now , i'm making change using javascript
 var element = document.getElementById('main-text');
 element.innerHTML = "Value By Javascript"
+
+//move the image 
+var img = document.getElementById('madi');
+img.onclick = function() {
+  img.style.marginLeft = '100px';  
+};
