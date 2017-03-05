5977f32723e6546fa0e5ec054fe51c26a8b0263c
HanikaVats <vatshanika@gmail.com>
2017-03-05 18:52:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,8 +7,8 @@ element.innerHTML = "Value By Javascript"
 var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight () {
-    marginLeft = marginLeft = 5;
-    img.style.marginLeft = marginLeft = 'px'
+    marginLeft = marginLeft + 5;
+    img.style.marginLeft = marginLeft + 'px'
 }
 img.onclick = function() {
     var interval = setInterval(moveRight, 50);
