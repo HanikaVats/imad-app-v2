b3da58ce414a50ee0f8149f81db6ae315e823480
HanikaVats <vatshanika@gmail.com>
2017-03-06 22:49:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,24 +1,25 @@
 //Counter code
 var button = document.getElementById('counter');
-
+var counter = 0;
 button.onclick = function() {
   // Create a request object
-    var request = new XMLHttpRequest();
+//    var request = new XMLHttpRequest();
   // Capture the response and store it in a variable
-  request.onreadystatechange = function () {
-      if (request.readyState ===XMLHttpRequest.DONE) {
+ // request.onreadystatechange = function () {
+   //   if (request.readyState ===XMLHttpRequest.DONE) {
           //Take some action
-          if (request.status === 200) {
-                var counter = request.responseText;
+   //       if (request.status === 200) {
+     //           var counter = request.responseText;
+              counter = counter + 1;
                 var span = document.getElementById('count');
                 span.innerHTL = counter.toString();
                   
-          }
-      }
+       //   }
+      //}
   
   //Not done yet
-  };
+ // };
   //Make a request
-  request.open('GET', 'http://hanikavats.imad.hasura-app.io/counter',true);
-  request.send(null);
+  //request.open('GET', 'http://hanikavats.imad.hasura-app.io/counter',true);
+ // request.send(null);
 };
