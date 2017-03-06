c013f6ab64a5f25157c77a48daeb0ac44dc6d7ad
HanikaVats <vatshanika@gmail.com>
2017-03-06 22:42:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,24 @@
-console.log('Loaded!');
-//now , i'm making change using javascript
-var element = document.getElementById('main-text');
-element.innerHTML = "Value By Javascript"
+//Counter code
+var button = document.getElementById('counter');
 
-//move the image 
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft = marginLeft + 'px'
-}
-img.onclick = function() {
-    var interval = setInterval(moveRight, 50);
-   
+button.onclick = function() {
+  // Create a request object
+    var request = new XMLHttpRequest();
+  // Capture the response and store it in a variable
+  request.onreadystatechange = function () {
+      if (request.readyState ===XMLHttpRequest.DONE) {
+          //Take some action
+          if (request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTL = counter.toString();
+                  
+          }
+      }
+  
+  //Not done yet
+  };
+  //Make a request
+  request.open('GET', 'http://hanikavats.imad.hasura-app.io/counter',true);
+  request.send(null);
 };
