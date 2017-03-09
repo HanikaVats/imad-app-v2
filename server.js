be0df354f19b5412c42e0b4a4de41244363232c7
HanikaVats <vatshanika@gmail.com>
2017-03-09 11:34:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,8 +94,8 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name/:name', function (req, res) {
-    var name = req.params.name;
+app.get('/submit-name', function (req, res) {//URL : /submit-name?name=xxxx
+    var name = req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
   
